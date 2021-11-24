da172025a22e526a3ef63fd597119dd3e61cfaeb
Likvidate <67824760+Likvidate@users.noreply.github.com>
2021-11-24 21:56:28 +0200

    Adding tailwind

--- src/main.js
+++ src/main.js
@@ -2,6 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueTailwind from 'vue-tailwind/dist/full'
+
+const settings = {
+  TInput: {
+    classes: 'border-2 block w-full rounded text-gray-800',
+    // ...More settings
+  },
+  TButton: {
+    classes: 'rounded-lg border block inline-flex items-center justify-center',
+    // ...More settings
+  },
+  // ...Rest of the components
+}
+
+Vue.use(VueTailwind, settings)
 
 Vue.config.productionTip = false
 
